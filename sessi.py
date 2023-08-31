955761fb2f8541b49a552f2c0409abd3892ddd70
JekSkeez <98408944+JekSkeez@users.noreply.github.com>
2023-08-31 23:37:37 +0700

    Update sessi.py

--- sessi.py
+++ sessi.py
@@ -3,6 +3,6 @@ from telethon.sync import TelegramClient
 api_id = '29212374'
 api_hash = '4bac9cabf7bf9b9c601374225b8ca206'
 
-with TelegramClient('session_name', api_id, api_hash) as client:
+with TelegramClient('session2', api_id, api_hash) as client:
     session_file = client.session.save()
     print("Файл сессии сохранен как:", session_file)
